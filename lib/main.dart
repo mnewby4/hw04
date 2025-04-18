3f44af9d9f3d112b4dce5266b481983eee77add1
mylanewby <167246119+mylanewby@users.noreply.github.com>
2025-04-17 20:29:06 -0400

    Splash screen implemented w separate login file

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,23 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
+import 'loginTabs.dart';
+/*
+  1. Any splash screen
+  2. User registration + login (firebase auth + cloud firestore)
+    - reg: unique user id, first, last, user role, registration datetime
+    - auth: email + password
+    auth pg shld have first, last, user role, email, pass
+  3. after: ordered list of all msg boards name w img icon = each board (hardcoded?)
+  4. icon in menu bar [of scaffold area] to open sliding nav menu
+    - msg boards
+    - profile [user can CHANGE this info!!]
+    - settings: logout, change login info w personal info 
+  5. new msg board -> list of all msgs posted in that convo 
+    - every msg: datetime, msg, user/displayname
+    - board name = titlebar
+    - msgs = displayed IN REAL TIME
+*/
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -30,338 +47,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 5), () {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: const TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.pages)),
-                Tab(icon: Icon(Icons.email)),
-              ],
-            ),
-            title: const Text('Activity 13 Demo'),
-          ),
-          body: TabBarView(
-            children: [
-              RegisterEmailSection(auth: _auth),
-              EmailPasswordForm(auth: _auth),
-            ],
-          ),
-        ),
-      ),
+    home: _isLoading ? SplashScreen() : LoginTabs(),
     );
   }
 }
 
-class RegisterEmailSection extends StatefulWidget {
-  RegisterEmailSection({Key? key, required this.auth}) : super(key: key);
-  final FirebaseAuth auth;
-
+class SplashScreen extends StatefulWidget {
+  SplashScreen({Key? key}) : super(key: key);
   @override
-  _RegisterEmailSectionState createState() => _RegisterEmailSectionState();
+  _SplashState createState() => _SplashState();
 }
 
-class _RegisterEmailSectionState extends State<RegisterEmailSection> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  bool _success = false;
-  bool _initialState = true;
-  String? _userEmail;
-
-  void _register() async {
-    try {
-      await widget.auth.createUserWithEmailAndPassword(
-        email: _emailController.text,
-        password: _passwordController.text,
-      );
-      setState(() {
-        _success = true;
-        _userEmail = _emailController.text;
-        _initialState = false;
-      });
-
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => AuthService(auth: widget.auth)),
-      );
-    } catch (e) {
-      setState(() {
-        _success = false;
-        _initialState = false;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            child: Text('Register with a new account'),
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-          ),
-          //Email textform
-          TextFormField(
-            controller: _emailController,
-            decoration: InputDecoration(labelText: 'Email'),
-            validator: (value) {
-              if (value?.isEmpty??true) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          //Password textform
-          TextFormField(
-            controller: _passwordController,
-            decoration: InputDecoration(labelText: 'Password'),
-            validator: (value) {
-              if(value?.isEmpty??true) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          //Submit Button
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            alignment: Alignment.center,
-            child: ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  _register();
-                }
-              },
-              child: Text('Submit'),
-            ),
-          ),
-          //Error Message
-          Container(
-            alignment: Alignment.center,
-            child: Text(
-              _initialState
-                  ? 'Please Register'
-              : _success
-                  ? 'Successfully registered $_userEmail'
-                  : 'Registration failed',
-              style: TextStyle(color: _success ? Colors.green : Colors.red),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class EmailPasswordForm extends StatefulWidget {
-  EmailPasswordForm({Key? key, required this.auth}) : super(key: key);
-  final FirebaseAuth auth;
-
-  @override
-  _EmailPasswordFormState createState() => _EmailPasswordFormState();
-}
-
-class _EmailPasswordFormState extends State<EmailPasswordForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  bool _success = false;
-  bool _initialState = true;
-  String _userEmail ='';
-
-  void _signInWithEmailAndPassword() async {
-    try {
-      await widget.auth.signInWithEmailAndPassword(
-        email: _emailController.text,
-        password: _passwordController.text,
-      );
-      setState(() {
-        _success = true;
-        _userEmail = _emailController.text;
-        _initialState = false;
-      });
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => AuthService(auth: widget.auth)),
-      );
-    } catch (e) {
-      setState(() {
-        _success = false;
-        _initialState = false;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            child: Text('Sign in with an existing account'),
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-          ),
-          TextFormField(
-            controller: _emailController,
-            decoration: InputDecoration(labelText: 'Email'),
-            validator: (value) {
-              if (value?.isEmpty??true) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: _passwordController,
-            decoration: InputDecoration(labelText: 'Password'),
-            validator: (value) {
-              if (value?.isEmpty??true) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            alignment: Alignment.center,
-            child: ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  _signInWithEmailAndPassword();
-                }
-              },
-              child: Text('Submit'),
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              _initialState
-                  ? 'Please sign in'
-                  : _success
-                  ? 'Successfully signed in $_userEmail'
-                  : 'Sign in failed',
-              style: TextStyle(color: _success ? Colors.green : Colors.red),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class AuthService extends StatefulWidget{
-  AuthService({Key? key, required this.auth});
-  final FirebaseAuth auth;
-
-  @override
-  _ProfilePage createState() => _ProfilePage();
-}
-
-class _ProfilePage extends State<AuthService> {
-  User? user = FirebaseAuth.instance.currentUser;
-  final TextEditingController _changedController = TextEditingController();
-  final GlobalKey<FormState> _changedKey = GlobalKey<FormState>();
-  Widget _welcomeInfo() {
-    return Text("Welcome, ${user!.email}");
-  }
-  Widget _passButton() {
-    return ElevatedButton(
-      onPressed: () => _changePassword(),
-      child: Text("Change Password"),
-    );
-  }
-
-  void _changePassword() {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        String errorMessage = " ";
-        return StatefulBuilder(
-          builder: (BuildContext context, StateSetter modalSetState) {
-            return Container(
-              height: 300,
-              padding: EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: _changedController,
-                    decoration: InputDecoration(labelText: "New Password"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      try {
-                        await user?.updatePassword(_changedController.text);
-                        modalSetState(() {
-                          errorMessage = "Successfully changed password";
-                        });
-                      } catch (e) {
-                        modalSetState(() {
-                          errorMessage = e.toString();
-                        });
-                      }
-                    },
-                    child: Text("Confirm"),
-                  ),
-                  Text(errorMessage),
-                ],
-              ),
-            );
-          },
-        );
-      },
-    );
-  }
-
-  void _signOut() async {
-    await widget.auth.signOut();
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text('Sign out in progress...'),
-    ));
-    await Future.delayed(const Duration(seconds: 1));
-    Navigator.pushAndRemoveUntil(
-      context,
-      MaterialPageRoute(builder: (context) => MyHomePage(title: 'Firebase Auth Demo')),
-      (route) => false,
-    );
-  }
-  
+class _SplashState extends State<SplashScreen>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Profile Page"),
-        actions: <Widget>[
-          // Sign out Button
-          ElevatedButton(
-            onPressed: () {
-              _signOut();
-            },
-            child: Text('Sign Out'),
-          ),
-        ],
-      ),
+      backgroundColor: Colors.blue,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            _welcomeInfo(),
-            _passButton(),
+            SizedBox(height: 250),
+            Text(
+              "ChattyRooms", 
+              style: TextStyle(
+                fontSize: 50,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 300,
+              child: Image.asset('assets/images/chaticon.png'),
+            ),
+            Text(
+              "Loading, please wait...",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+              ),
+            ),
           ],
         ),
       ),
